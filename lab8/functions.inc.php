4ee2c4c98710c78acee5544de1745741f6bb3c32
hsiloof <1241571112@qq.com>
2019-05-21 22:05:00 +0800

    information

--- lab8/functions.inc.php
+++ lab8/functions.inc.php
@@ -1,8 +1,15 @@
 <?php
 
     function outputOrderRow($file, $title, $quantity, $price) {
+        $newPrice=number_format($price,2);
+        $amount=$quantity*$newPrice;
+        $newAmount=number_format($amount,2);
         echo "<tr>";
-        //TODO
+        echo "<td> <img src='./images/books/tinysquare/$file'> </td>";
+        echo "<td> $title </td>";
+        echo "<td> $quantity </td>";
+        echo "<td> $$newPrice </td>";
+        echo "<td> $$newAmount </td>";
         echo "</tr>";
     }
 ?>
